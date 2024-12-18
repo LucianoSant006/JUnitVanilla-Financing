181493bd71f3bf27bdc0c0ac951ed1c8874c8046
LucianoSant006 <santiagoluciano383@gmail.com>
2024-12-17 21:15:44 -0300

    implements @Test constructorShouldInstantiateWhenDataIsValid

--- src/tests/entities/FinancingTests.java
+++ src/tests/entities/FinancingTests.java
@@ -1,8 +1,22 @@
 package tests.entities;
 
+import entities.Financing;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FinancingTests {
-    
+
+
+    @Test
+    public void constructorShouldInstantiateWhenDataIsValid(){
+
+        Financing fn = new Financing(100000.00, 4000.00, 90);
+
+        Assertions.assertEquals(100000.00,fn.getTotalAmount());
+        Assertions.assertEquals(90,fn.getMonths());
+        Assertions.assertEquals(4000.00,fn.getIncome());
+
+
+    }
 
 }
